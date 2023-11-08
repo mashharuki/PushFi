a7cf7a82af3231068292e70576b607f53ebec073
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-11-08 19:50:46 +0900

    ゲーム情報を更新

--- pkgs/backend/scripts/wakuwakuGame/create-game.ts
+++ pkgs/backend/scripts/wakuwakuGame/create-game.ts
@@ -8,10 +8,10 @@ async function main() {
   // Deployed Contract address
   const contractAddress = "0x985e632298882212d91AB2C9c0d00D80b82880b7"
   // Game用の変数
-  const gameName = "SampleGame";
-  const goalCount = 10;
+  const gameName = "SampleGame2";
+  const goalCount = 5;
   const prizeToken = "0x045aa885e04dab32316eA0B39Cda9c966A5d9845"; // test USDC
-  const prizeValue = ethers.utils.parseEther("100");
+  const prizeValue = ethers.utils.parseEther("10");
   const nftAddress = "0x36C1b81EB093aEdc6fF4288E7f8b12C400E97820"
 
   const game = await ethers.getContractAt('WakuWakuGame', contractAddress);
