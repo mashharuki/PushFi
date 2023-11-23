3844176dff13bed4441269c7060c4e1246c5fe19
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-11-23 10:40:30 +0900

    設定を諸々変更しました。

--- pkgs/frontend/src/pages/_document.tsx
+++ pkgs/frontend/src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
