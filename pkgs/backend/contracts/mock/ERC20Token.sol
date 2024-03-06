87eaa08a5bfc7a4838650628309e2ecf08c55887
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-03-06 23:43:17 +0900

    修正

--- pkgs/backend/contracts/mock/ERC20Token.sol
+++ pkgs/backend/contracts/mock/ERC20Token.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDCToken is ERC20 {
-    constructor() ERC20("USDC Token", "USDC") {
-      _mint(msg.sender, 10000 ether);
-    }
+  constructor() ERC20("USDC Token", "USDC") {
+    _mint(msg.sender, 10000 ether);
+  }
 
-    function faucet(address recipient, uint256 amount) external {
-      _mint(recipient, amount);
-    }
+  function faucet(address recipient, uint256 amount) external {
+    _mint(recipient, amount);
+  }
 }
